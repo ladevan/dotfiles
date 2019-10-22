705cf84ca5a4f389d9b6a800f7d6764892c8f92f
dengyu <dxl5849@rit.edu>
2019-10-21 22:20:50 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# setup vim related stuff
+echo "create backup, swap and undo folder"
+mkdir -p .vim/.backup
+mkdir -p .vim/.swp
+mkdir -p .vim/.undo
 echo "downloading .vimrc"
 curl -O https://raw.githubusercontent.com/ladevan/dotfiles/master/.vimrc
 
 # get prezto
-if [ -s ~/.zprezto ];then
+if [ -s ~/.zpreztorc ]
+then
   echo "prezto installed"
 else
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
