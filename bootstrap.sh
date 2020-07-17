4323b2f7bf41a236120db08eb1bbf5ce8565696f
dengyu <ldy1985@gmail.com>
2020-07-17 13:58:28 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,15 +14,12 @@ echo "get dircolor visualization script"
 curl -O https://raw.githubusercontent.com/ladevan/dotfiles/master/colours.sh
 echo "get dircolor scheme"
 curl -O https://raw.githubusercontent.com/ladevan/dotfiles/master/.dir_colors
-
+echo "get .zshrc"
+curl -O https://raw.githubusercontent.com/ladevan/dotfiles/master/.zshrc
 # get prezto
 if [ -f .zpreztorc ]
 then
   echo "prezto installed"
 else
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-  setopt EXTENDED_GLOB
-  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-  done
 fi
