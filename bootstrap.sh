767b4b496b3cef7d1cdbb3832ce15f10cfb29954
dengyu <ldy1985@gmail.com>
2020-07-22 18:22:05 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # setup vim related stuff
 echo "create backup, swap and undo folder"
@@ -22,4 +22,11 @@ then
   echo "prezto installed"
 else
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
 fi
+
+
+
